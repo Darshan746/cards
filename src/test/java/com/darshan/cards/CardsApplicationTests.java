fa1213b220647b0a7f7b4bfad8af0a10565e8be5
darshan <darshan.mn746@gmail.com>
2023-07-25 20:59:18 +0530

    cards ms

---
+++ src/test/java/com/darshan/cards/CardsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.darshan.cards;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
